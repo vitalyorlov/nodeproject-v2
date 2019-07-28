62467b0cc822e5275ca85b6ebdd9b9a69b5ad253
vitalyorlov <vitali.arlou@netcracker.com>
2019-07-28 21:18:28 +0300

    Mysql connection has been added

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const calc = require('./modules/calc');
-const _ = require('lodash');
 const fs = require('fs');
-const safeFileRead = require('./modules/file-reader').safeFileRead;
-const http = require('http');
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
-
-console.log('Application is ready');
-console.log('Number: ', calc.sum(2, 5));
-console.log('Lodash: ', _.assign({ a: 1 }, { b: 3 }));
 const env = getEnv();
-//safeFileRead('files/content.txt');
+var mysql = require('mysql');
+var mysqlConnection = mysql.createConnection(env.mysql);
+
+mysqlConnection.connect((err) => {
+    console.log(err ? err : 'Database is connected');
+});
 
 app.use((req, res, next) => {
     console.log(req.headers);
@@ -23,17 +20,29 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', (request, response) => {
-    response.render('home', {
-        name: 'Vitaly'
+
+    mysqlConnection.query('select * from users', (err, rows, fields) => {
+        mysqlConnection.end();
+        
+        if (err) {
+            console.log('SWW');
+            return;
+        }
+
+        console.log('Rows: ', rows);
+
+        response.render('home', {
+            name: rows[0].title
+        });
     });
 });
+
 app.get('/error', (req, res) => { throw new Error('Error') });
 
 app.listen(env.port, (err) => {
     err && console.log(`Unable to create dev server`);
     console.log(`Listen port ${env.port}...`);
 });
-
 
 function getEnv() {
     let content;
