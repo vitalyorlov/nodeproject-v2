c3f6435db212c09d5df7cf442244d06e23c19e92
vitalyorlov <vitali.arlou@netcracker.com>
2019-07-28 16:04:08 +0300

    Handlebars layouts have been added

--- app.js
+++ app.js
@@ -2,12 +2,38 @@ const calc = require('./modules/calc');
 const _ = require('lodash');
 const fs = require('fs');
 const safeFileRead = require('./modules/file-reader').safeFileRead;
+const http = require('http');
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 console.log('Application is ready');
 console.log('Number: ', calc.sum(2, 5));
-console.log('Lodash: ', _.assign({a: 1}, {b: 3}));
-console.log('Env: ', getEnv());
-safeFileRead('files/content.txt');
+console.log('Lodash: ', _.assign({ a: 1 }, { b: 3 }));
+const env = getEnv();
+//safeFileRead('files/content.txt');
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    next();
+});
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (request, response) => {
+    response.render('home', {
+        name: 'Vitaly'
+    });
+});
+app.get('/error', (req, res) => { throw new Error('Error') });
+
+app.listen(env.port, (err) => {
+    err && console.log(`Unable to create dev server`);
+    console.log(`Listen port ${env.port}...`);
+});
+
 
 function getEnv() {
     let content;
@@ -18,3 +44,4 @@ function getEnv() {
     }
     return JSON.parse(content);
 }
+
